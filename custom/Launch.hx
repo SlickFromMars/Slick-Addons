8468951b1037dc158d9619bd25a39bab25ea439b
SlickFromMars <minecraftsummer2017@gmail.com>
2022-01-24 13:01:03 -0500

    open url code

---
+++ custom/Launch.hx
@@ -0,0 +1,24 @@
+package custom;
+
+import flixel.FlxG;
+import openfl.utils.Assets;
+import lime.utils.Assets as LimeAssets;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#else
+import openfl.utils.Assets;
+#end
+
+class Launch
+{
+    public static function openURL(site:String) {
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [site]);
+		#else
+		FlxG.openURL(site);
+		#end
+	}
+}
