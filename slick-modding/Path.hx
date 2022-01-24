19f174e2c0f5781699d322e3fb6229e7bb579547
SlickFromMars <minecraftsummer2017@gmail.com>
2022-01-24 09:55:32 -0500

    Pather

---
+++ slick-modding/Path.hx
@@ -0,0 +1,76 @@
+package slick-modding;;
+
+import haxe.io.Path;
+import flixel.FlxSubState;
+import flixel.FlxG;
+import flixel.graphics.frames.FlxAtlasFrames;
+import openfl.utils.AssetType;
+import openfl.utils.Assets as OpenFlAssets;
+import lime.utils.Assets;
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+
+using StringTools;
+
+class Path {
+	inline public static var SOUND_EXT:String = #if web "mp3" #else "ogg" #end;
+
+	public static var EXT_MAP:Map<String, String> = [
+		'SOUND' => SOUND_EXT,
+		'MUSIC' => SOUND_EXT,
+		'VIDEO' => "mp4",
+		'IMAGE' => 'png',
+		'XML' => 'xml',
+		'FONT' => 'ttf',
+		'JSON' => 'json',
+		'TEXT' => 'txt'
+	];
+
+	public static var FOLDER_MAP:Map<String, String> = [
+		'sound' => 'sounds',
+		'music' => 'music',
+		'video' => 'videos',
+		'image' => 'images',
+		'xml' => 'images',
+		'font' => 'fonts',
+		'json' => 'data',
+		'text' => 'data'
+	];
+
+	inline static public function getPath(key:String, ?type:String = 'none', ?library:String)
+	{
+		var EXT:String = '';
+		if (type != 'none') EXT = '.' + EXT_MAP.get(type.toUpperCase());
+
+		var FOLDER:String = FOLDER_MAP.get(type.toLowerCase());
+		var LIB:String = '';
+
+		if (library != null)
+			FOLDER = '$library/';
+
+		var PATH:String = '$LIB$FOLDER/$key$EXT';
+		var pathPrefix:String = 'assets/';
+
+		#if debug trace('getting something from $pathPrefix$PATH'); #end
+			
+		if (!FileSystem.exists(pathPrefix + PATH) && library == 'none') {
+			return getPath('$key$EXT', 'none', 'dynamic');
+		}
+		return pathPrefix + PATH;
+	}
+
+	inline static public function fileExists(key:String, type:String, ?library:Null<String>)
+	{
+		if(OpenFlAssets.exists(Path.getPath(key, type, library))) {
+			return true;
+		}
+		return false;
+	}
+
+	inline static public function getSparrowAtlas(key:String, ?library:String)
+	{
+		return FlxAtlasFrames.fromSparrow(getPath(key, 'image', library), getPath(key, 'xml', library));
+	}
+}
