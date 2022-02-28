4e5d6ec247b6b31d254586c5229dd1a4e16035f9
SlickFromMars <minecraftsummer2017@gmail.com>
2022-02-27 20:36:05 -0500

    Attached Sprites

---
+++ custom/AttachedSprite.hx
@@ -0,0 +1,37 @@
+package;
+
+import flixel.FlxSprite;
+
+/**
+ * A sprite that will track and stick to a parent sprite.
+ */
+class AttachedSprite extends FlxSprite
+{
+    /**
+     * The sprite to track.
+     */
+    public var sprTracker:FlxSprite;
+
+    /**
+     * The x offset of the object.
+     */
+    public var myX:Float = 0;
+
+    /**
+     * The y offset of the object.
+     */
+    public var myY:Float = 0;
+
+    public function new() {
+        super();
+    }
+
+    override function update(elapsed:Float) {
+        super.update(elapsed);
+
+        if(sprTracker != null) {
+            setPosition(sprTracker.x + myX, sprTracker.y + myY);
+            scrollFactor.set(sprTracker.scrollFactor.x, sprTracker.scrollFactor.y);
+        }
+    }
+}
