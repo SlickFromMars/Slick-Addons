6cbe2fed8272ed40435a399ab9e82cf2beae78ae
SlickFromMars <minecraftsummer2017@gmail.com>
2022-02-28 10:03:55 -0500

    moved these

--- sample/source/PlayState.hx
+++ sample/source/PlayState.hx
@@ -1,14 +1,12 @@
 package;
 
 import custom.Web;
-import custom.AttachedSprite;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
 	var logo:FlxSprite;
-	var attatchedText:AttachedSprite;
 
 	override public function create()
 	{
