03a60ba83aef12b9f8ca6777dd0be6a03c7a0eef
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-01 18:07:12 -0500

    the unfinished sandwich

--- custom/animation/AtlasFrameConstuctor.hx
+++ custom/animation/AtlasFrameConstuctor.hx
@@ -1,11 +1,15 @@
 package custom.animation;
 
 import flixel.graphics.frames.FlxFramesCollection;
+import flixel.graphics.frames.FlxFrame;
 
 class AtlasFrameConstructor
 {
 	public static function construct(origin:String, ?excludeArray:Array<String>):FlxFramesCollection
 	{
-		return null;
+		var frameCollection:FlxFrameCollection;
+		var frameArray:Array<Array<FlxFrame>> = [];
+
+		return frameCollection;
 	}
 }
