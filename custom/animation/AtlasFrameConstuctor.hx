efabf71ac9c03b92a9b7cb7784884f975f5370fc
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-01 17:43:34 -0500

    another overkill feature

---
+++ custom/animation/AtlasFrameConstuctor.hx
@@ -0,0 +1,11 @@
+package custom.animation;
+
+import flixel.graphics.frames.FlxFramesCollection;
+
+class AtlasFrameConstructor
+{
+	public static function construct(origin:String, ?excludeArray:Array<String>):FlxFramesCollection
+	{
+		return null;
+	}
+}
