658ae2889952fd2882acdbef3b2b84885b537d4b
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-04 14:51:11 -0500

    This might work

--- sample/source/Main.hx
+++ sample/source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import custom.SlickAddons;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
@@ -9,5 +10,7 @@ class Main extends Sprite
 	{
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
+
+		SlickAddons.checkIfOutdated();
 	}
 }
---
+++ sample/source/custom/SlickAddons.hx
@@ -0,0 +1,58 @@
+package custom;
+
+import custom.util.SemVer;
+import haxe.Json;
+
+class SlickAddons
+{
+	private static var frameWorkVersion:String = '2.0.0';
+
+	private static var hasChecked:Bool = false;
+
+	public static function checkIfOutdated()
+	{
+		var sucessful:Bool;
+		var http = new haxe.Http("https://raw.githubusercontent.com/SlickFromMars/Slick-Addons/main/haxelib.json");
+
+		http.onData = function(data:String)
+		{
+			var meta:HaxelibJSON = Json.parse(StringTools.trim(data));
+
+			var before:SemVer = SemVer.fromString(frameWorkVersion);
+			var after:SemVer = SemVer.fromString(meta.version);
+
+			switch (before.compare(after))
+			{
+				case 1:
+					trace('Current version is newer. WTF?');
+				case -1:
+					trace('Current Slick Addons version is outdated.');
+				case 0:
+					trace('Slick Addons is up to date.');
+			}
+		}
+
+		http.onError = function(error)
+		{
+			trace('ERROR GETTING SLICK ADDONS META!! Check your router ya dingus');
+		}
+
+		http.request();
+	}
+}
+
+// this is stupid sorry :(
+typedef HaxelibJSON =
+{
+	name:String,
+	url:String,
+	license:String,
+	tags:Array<String>,
+	description:String,
+	version:String,
+	releasenote:String,
+	contributors:Array<String>,
+	?dependencies:Array<Dynamic>,
+	?classPath:String,
+	?main:String
+}
