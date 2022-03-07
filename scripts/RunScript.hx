0f29b4138c977368e1d3d5af545073ce7994b79f
SlickFromMars <minecraftsummer2017@gmail.com>
2022-03-07 12:19:15 -0500

    rick

--- scripts/RunScript.hx
+++ scripts/RunScript.hx
@@ -1,11 +1,13 @@
 package scripts;
 
 import custom.SlickAddons;
+import custom.Web;
 
 class RunScript
 {
 	public static function main()
 	{
 		SlickAddons.checkIfOutdated();
+		Web.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 	}
 }
